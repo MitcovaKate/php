9c5ac92f81aae2031cc84b839c7ccbab7344baee
Mitcova Kate <katya.mitkova@bk.ru>
2024-02-20 22:36:18 +0200

    Update index.php

--- booking-database/src/index.php
+++ booking-database/src/index.php
@@ -1,10 +1,14 @@
+
 <?
 define('PAGE_TITLE', 'Booking confirmation');
 define('SEAT_PRICE', 100.50);
 
 $book_client_vip=true;
 $book_adults=3;
-$book_coast= floatval($book_adults * SEAT_PRICE);
+$book_coast= $book_adults * SEAT_PRICE;
+$formatted_coast = '$' . number_format($book_coast, 2, '.', ',');
+$formatted_price ='$' . number_format(SEAT_PRICE, 2, '.', ',');
+
 ?>
 
 
@@ -12,7 +16,7 @@ $book_coast= floatval($book_adults * SEAT_PRICE);
 
   <h1><?= PAGE_TITLE ?></h1>
    <p>Adults:<?= $book_adults ?></p>
-   <p>Total coast: <?= $book_adults ?> x <?= SEAT_PRICE ?> = <?= $book_coast ?></p>
+   <p>Total coast: <?= $book_adults ?> x <?= $formatted_price ?> = <?= $formatted_coast ?></p>
 
    <? if($book_client_vip ===true): ?> 
    <p>VIP:</p>
