30a970e4a364b5e953fce2b25a2a6699baa8866b
Mitcova Kate <katya.mitkova@bk.ru>
2024-02-28 22:21:20 +0200

    Create index.php

---
+++ loops/if.else/index.php
@@ -0,0 +1,12 @@
+<?
+$ad_views =rand(0,5);
+
+print($ad_views);
+
+// if($ad_views < 4){
+//     print ("New advertisment text ...");
+// }
+if($ad_views >=0 && $ad_views<=3){
+    print ("New advertisment text ...");
+}
+
