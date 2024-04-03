0ddea40d2de1d2fabc02a8c3549ff1fe7139b4a4
Mitcova Kate <katya.mitkova@bk.ru>
2024-04-04 00:02:24 +0300

    Update reader.php

--- write/read/reader.php
+++ write/read/reader.php
@@ -1,5 +1,10 @@
+// <?
+// $file = fopen("./client.txt","r");
+// $name = fread($file,100);
+// print($name);
+// ?>
 <?
-$file = fopen("./client.txt","r");
-$name = fread($file,100);
-print($name);
+$file = fopen("./data/client.json","r");
+$client =json_decode(fread($file,1000),true);
+var_dump($client);
 ?>
