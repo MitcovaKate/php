4a28d05cbdb78bcd3c05785a6f93f4ed115839fe
Mitcova Kate <katya.mitkova@bk.ru>
2024-05-02 00:43:05 +0300

    Update index.php

--- sea-battle/game_app/index.php
+++ sea-battle/game_app/index.php
@@ -1,6 +1,19 @@
-<? require_once 'input.php' ?>
-
-<link rel="stylesheet" href="/css/app.css">
-
-<? require_once 'map.php' ?>
-<? require_once 'template.php' ?>
+<?require_once 'lib.php'?>
+<?require_once 'map.php' ?>
+<? $map = load_map() ?>
+<? $coords = get_coords($_GET)?>
+<?$map = shoot($map,$coords)?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>SEA BATTLE</title>
+</head>
+<body>
+    <div class="container">
+        <?= render_map($map)?>
+    </div>
+</body>
+</html>
+<? save_map($map) ?> 
